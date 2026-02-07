1cc323b2f224acc2cd31b62a2fef1452b3a3bb6e
iam-suresh077 <129142479+iam-suresh077@users.noreply.github.com>
2026-02-07 12:08:20 +0530

    Add ArgoCD namespace and Helm release configuration

---
+++ argocd.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  values = [<<EOF
+server:
+  service:
+    type: LoadBalancer
+EOF
+  ]
+}
