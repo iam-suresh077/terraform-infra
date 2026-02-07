e9d927cd86dde5db38eb8f604b47dc0c36918343
iam-suresh077 <129142479+iam-suresh077@users.noreply.github.com>
2026-02-07 12:06:40 +0530

    Create eks-test.tf

---
+++ eks-test.tf
@@ -0,0 +1,17 @@
+module "eks_test" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "eks-test"
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 2
+      instance_types = ["t3.medium"]
+    }
+  }
+}
